781383f14872d9bc0128b96f9ba8879a6c01d07e
MRXbef <adambayu878@gmail.com>
2024-08-26 18:07:25 +0800

    vercel config

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -81,7 +81,9 @@ export const Login = async(req, res) => {
 
 export const Logout = async(req, res) => {
     const { refreshToken } = req.cookies;
-    if (!refreshToken) return res.status(204).end();
+    if (!refreshToken) {
+        return res.status(401).json({ msg: "No refresh token provided" });
+    }
 
     try {
         const user = await Users.findOne({ where: { refreshToken: refreshToken } });
