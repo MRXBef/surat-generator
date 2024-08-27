e850d970e739a1248afe325f9c5e32b373183e03
MRXbef <adambayu878@gmail.com>
2024-08-27 20:54:12 +0800

    verify token fix kalau akun nabrak maka setiap request akan di cek

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,9 +1,15 @@
 import jwt from "jsonwebtoken";
+import Users from "../models/userModels";
 
 export const verifyToken = async(req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if(token == null) return res.sendStatus(401)
+
+    const user = await Users.findOne({
+        where: {refreshToken: token}
+    })
+    if(!user) return res.sendStatus(401)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err) res.sendStatus(403)
