a5bb83167a673a8df42e6c6c85160a62b7865e0b
MRXbef <adambayu878@gmail.com>
2024-08-27 21:02:31 +0800

    rollback x2

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,15 +1,9 @@
 import jwt from "jsonwebtoken";
-import Users from "../models/userModels.js";
 
 export const verifyToken = async(req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if(token == null) return res.sendStatus(401)
-
-    const user = await Users.findOne({
-        where: {refreshToken: token}
-    })
-    if(!user) return res.sendStatus(401)
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err) res.sendStatus(403)
