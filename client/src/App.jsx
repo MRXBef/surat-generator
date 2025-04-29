d6213fbada588bead20bbdf6d0b0af6b0591f4b1
MRXbef <adambayu878@gmail.com>
2025-04-29 15:07:52 +0800

    update fitur login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Payment from './components/Payment';
+import LoginPage from './components/LoginPage';
 import Dashboard from './components/Dashboard';
 import Laporan from './components/Laporan';
 import EditSurat from './components/EditSurat';
